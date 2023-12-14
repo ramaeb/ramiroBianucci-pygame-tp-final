9ca8a09353761aa9ecef4d710eba62c9375c8c38
ramaeb <bianucciramiro@hotmail.com>
2023-12-14 20:16:23 -0300

    modified:   main.py 	deleted:    ramiroBianucci-pygame-tp-final-codigo.zip 	new file:   ramiroBianucci-pygame-tp-final.zip

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from models.player.main_player import Jugador
 screen = pg.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA))
 
 clock = pg.time.Clock()
-
+fuente = pg.font.SysFont("Arial",30)
 
 fruta_grupo = pg.sprite.Group()
 fruta = Item(100,600,fruta_img)
@@ -56,21 +56,29 @@ while juego_ejecutandose:
     form_activo.draw()
     form_activo.upd     
     '''
-    
+
     screen.blit(back_img, back_img.get_rect())
     mundo.draw(screen)
 
     #screen.blit(back_img, back_img.get_rect())
     #centralizar en nivel(class) con metodo draw
-    jugador.draw(screen)
-    enemigo.draw(screen)
-    enemigo.update()
-    jugador.update(mundo)
-    bullet_group.draw(screen)
-    bullet_group.update(jugador,enemigo)
-    fruta_grupo.draw(screen)
-    fruta_grupo.update(jugador)
-    nivel.update()
+    if jugador.jugador_vivo == False:
+            mensaje_muerte = fuente.render("MUERTO",False,(0,0,0))
+            screen.blit(mensaje_muerte,(350,500))
+            jugador.draw(screen)
+            jugador.update(mundo)
+            nivel.update()
+            cancion_fx.fadeout(5)
+    else:
+        enemigo.draw(screen)
+        enemigo.update()
+        jugador.draw(screen)
+        jugador.update(mundo)
+        bullet_group.draw(screen)
+        bullet_group.update(jugador,enemigo)
+        fruta_grupo.draw(screen)
+        fruta_grupo.update(jugador)
+        nivel.update()
 
     #MANEJO DE VIDAS !!
     
